1d1857b4da8b258e7df688fdb6bde1d186905ac7
MingQiao <Qm7895123>
2017-10-20 18:51:29 +0800

    first commit

---
+++ src/demo/MutliThread.java
@@ -0,0 +1,17 @@
+package demo;
+
+public class MutliThread extends Thread {
+    private int ticket=100;//每个线程都拥有100张票
+
+    public MutliThread (){}
+    public MutliThread (String name){
+        super(name);
+    }
+
+    @Override
+    public void run() {
+        while(ticket>0){
+            System.out.println(ticket--+" is saled by "+Thread.currentThread().getName());
+        }
+    }
+}
---
+++ src/demo/Person.java
@@ -0,0 +1,71 @@
+package demo;
+
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private String job;
+    private String gender;
+    private int salary;
+    private int age;
+
+    public Person(String firstName, String lastName, String job,
+                  String gender, int age, int salary)       {
+        this.setFirstName(firstName);
+        this.setLastName(lastName);
+        this.setGender(gender);
+        this.setAge(age);
+        this.setJob(job);
+        this.setSalary(salary);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+// Getter and Setter
+// . . . . .
+}
